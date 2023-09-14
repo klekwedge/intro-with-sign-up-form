2fba6cf1ea1e6272bdcb81c97e65450587648fe7
Andrey <antonov.andrey.10101@gmail.com>
2023-09-14 13:48:25 +0300

    feat: add styles

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,23 @@ export default {
 </script>
 
 <style scoped>
+.content {
+		display: flex;
+		align-items: center;
+		margin: 0 auto;
+		max-width: 1400px;
+		padding-top: 200px;
+	}
+
+	.content > div {
+		flex: 0 1 50%;
+	}
+
+	@media (max-width: 1200px) {
+		.content {
+			padding-top: 80px;
+			gap: 3rem;
+			flex-direction: column;
+		}
+	}
 </style>
