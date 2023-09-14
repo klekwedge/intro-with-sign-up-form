6af9f4be22a901b13028a98f57a10cf75a52164d
Andrey <antonov.andrey.10101@gmail.com>
2023-09-14 13:50:35 +0300

    feat: add InputComponent

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>Hello</div>
+  <div class="content">
+    <InputComponent />
+  </div>
 </template>
 
-<script lang="ts">
+<script >
+import InputComponent from "./components/InputComponent.vue";
+
 export default {
+  components: { InputComponent },
   setup() {
     return {};
   },
@@ -12,22 +17,22 @@ export default {
 
 <style scoped>
 .content {
-		display: flex;
-		align-items: center;
-		margin: 0 auto;
-		max-width: 1400px;
-		padding-top: 200px;
-	}
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  max-width: 1400px;
+  padding-top: 200px;
+}
 
-	.content > div {
-		flex: 0 1 50%;
-	}
+.content > div {
+  flex: 0 1 50%;
+}
 
-	@media (max-width: 1200px) {
-		.content {
-			padding-top: 80px;
-			gap: 3rem;
-			flex-direction: column;
-		}
-	}
+@media (max-width: 1200px) {
+  .content {
+    padding-top: 80px;
+    gap: 3rem;
+    flex-direction: column;
+  }
+}
 </style>
---
+++ src/components/InputComponent.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="input-block">
+    <input
+      :type="type"
+      :class="{ error: error }"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+    <span v-if="error" class="error-msg"
+      >The {{ placeholder }} cannot be empty
+    </span>
+    <img
+      v-if="error"
+      class="icon-error"
+      src="/images/icon-error.svg"
+      alt="error icon"
+    />
+  </div>
+</template>
+<script setup>
+const { type, placeholder, modelValue, error } = defineProps([
+  "type",
+  "placeholder",
+  "modelValue",
+  "error",
+]);
+</script>
+
+<style scoped>
+input {
+  padding: 15px;
+  border-radius: 5px;
+  border: 2px solid var(--graylishBlue);
+  font-size: 16px;
+  padding-left: 25px;
+  font-weight: 500;
+  position: relative;
+  margin-bottom: 10px;
+}
+
+.input-block {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+input:focus {
+  outline: none !important;
+  border: 2px solid var(--blue);
+}
+
+.error {
+  border: 2px solid var(--red);
+}
+
+.error-msg {
+  font-style: italic;
+  font-size: 12px;
+  text-align: right;
+  color: var(--red);
+  font-weight: 600;
+  position: absolute;
+  bottom: -10px;
+  right: 0;
+}
+
+.icon-error {
+  position: absolute;
+  right: 15px;
+  top: 18px;
+}
+</style>
