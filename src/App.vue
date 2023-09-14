f69462a8714a243887c959c6b31ea56537824fd2
Andrey <antonov.andrey.10101@gmail.com>
2023-09-14 13:58:27 +0300

    feat: add FreeTrial

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Input />
     <SubmitButton />
     <Introduction />
+    <FreeTrial />
   </div>
 </template>
 
@@ -10,9 +11,10 @@
 import Input from "./components/Input.vue";
 import SubmitButton from "./components/SubmitButton.vue";
 import Introduction from "./components/Introduction.vue";
+import FreeTrial from "./components/FreeTrial.vue";
 
 export default {
-  components: { Input, SubmitButton, Introduction },
+  components: { Input, SubmitButton, Introduction, FreeTrial },
   setup() {
     return {};
   },
---
+++ src/components/FreeTrial.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="toast shadow">
+    <b>Try it free 7 days</b> then $20/mo. thereafter
+  </div>
+</template>
+<style scoped>
+.toast {
+  color: var(--white);
+  background-color: var(--blue);
+  text-align: center;
+  padding: 15px 0;
+  display: block;
+  border-radius: 10px;
+}
+
+.toast:hover {
+  cursor: pointer;
+  background-color: var(--bluePointer);
+}
+</style>
